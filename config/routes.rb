7d30111d3c407d51b1ae7f82fd9527cda6a8bc88
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-04-13 15:51:14 -0400

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
-  post "restaurants", to: "restaurants#create"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+# get "restaurants", to: "restaurants#index"
+# get "restaurants/new", to: "restaurants#new"
+# get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+# post "restaurants", to: "restaurants#create"
+# get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
